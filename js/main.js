84e868e36fd2618398fec855e522be63d66e1972
Ernest Iliiasov <me@ernest.asia>
2018-01-12 18:46:22 -0500

    Add comments to js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Open cart and add item when the "Add to cart" button is clicked */
 $('#addtocart').click(function() {
 
   /* Render the new cart item */
@@ -10,13 +11,18 @@ $('#addtocart').click(function() {
   </div>
   `;
 
+  /* Add the item to cart */
   $('#cart_items').append(template);
+
+  /* Scroll cart to the bottom (so the new item is visible) */
   $('#cart').scrollTop($('.cart__inner').height());
 
+  /* Show the cart after a quick delay */
   setTimeout(function() {
     $('body').toggleClass('cart-shown');
   }, 50);
 
+  /* Remove .added class from the new added item, so it will not animate  */
   setTimeout(function() {
     $('.cart_item').removeClass('added');
   }, 1000);
